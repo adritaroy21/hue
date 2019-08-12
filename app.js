576141e29b9f3eae86fe033bfb77a9d64f5d4a86
adroy98 <adrita.roy2104@gmail.com>
2019-08-12 19:42:05 +0530

    Latest update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const copy = document.getElementById('copy');
 const message = document.getElementById('message');
 
 hex.readOnly = true;
-generate.addEventListener('click', generateNew);
+generate.addEventListener('click', randomColour);
 copy.addEventListener('click', copyHex);
 
 // Returns a number between 0 and 255
@@ -38,17 +38,13 @@ document.addEventListener('DOMContentLoaded', function() {
   randomColour();
 });
 
-// Reloads the page
-function generateNew() {
-  randomColour();
-}
-
 //Copy Hex to clipboard
 function copyHex() {
   hex.focus();
   hex.select();
   document.execCommand('copy');
 
+  //Shows message for a brief amount of time
   message.style.display = 'block';
   setTimeout(function() {
     message.style.display = 'none';
