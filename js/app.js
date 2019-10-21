7d1dc5f80a7d203a108cbcaae72559f0078f40de
Aritra Mukherjee <aritramukherjee100@gmail.com>
2019-10-21 22:26:26 +0530

    major update: display generated color info, maintain history of generated colors, persist history to localstorage

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
 const colour = document.getElementById('colour');
 const generate = document.getElementById('generate');
 const hex = document.getElementById('hex');
-const copy = document.getElementById('copy');
+const copy = document.getElementById('primaryCopyBtn');
 const message = document.getElementById('message');
+const historyContent = document.getElementById('history-content');
+
+const name = document.getElementById('name');
+const rgb = document.getElementById('rgb');
+const cmyk = document.getElementById('cmyk');
+const hsv = document.getElementById('hsv');
+const hsl = document.getElementById('hsl');
+const xyz = document.getElementById('xyz');
+const status = document.getElementById('status');
+const fields = [name, rgb, cmyk, hsv, hsl, xyz];
+
+let hexHistoryQueue = [];
 
 hex.readOnly = true;
-generate.addEventListener('click', randomColour);
-copy.addEventListener('click', copyHex);
+
+// Get existing history from LS if any
+function getHistoryFromStorage() {
+  if (localStorage.getItem('colors')) {
+    hexHistoryQueue = JSON.parse(localStorage.getItem('colors'));
+  } else {
+    localStorage.setItem('colors', '');
+  }
+}
 
 // Returns a number between 0 and 255
 function randomNumber() {
@@ -22,6 +41,89 @@ function rgbToHex(value) {
   return hex;
 }
 
+// Update info area
+function updateInfoArea(data) {
+  if (data) {
+    status.innerHTML = `(online)`;
+    status.setAttribute('class', 'text-success');
+    name.innerHTML = `${data.name.value}`;
+    rgb.innerHTML = `${data.rgb.r}, ${data.rgb.g}, ${data.rgb.b}`;
+    cmyk.innerHTML = `${data.cmyk.c}, ${data.cmyk.m}, ${data.cmyk.y}, ${data.cmyk.k}`;
+    hsv.innerHTML = `${data.hsv.h}, ${data.hsv.s}%, ${data.hsv.v}%`;
+    hsl.innerHTML = `${data.hsl.h}, ${data.hsl.s}%, ${data.hsl.l}%`;
+    xyz.innerHTML = `${data.XYZ.X}, ${data.XYZ.Y}, ${data.XYZ.Z}`;
+  } else {
+    status.innerHTML = `(offline)`;
+    status.setAttribute('class', 'text-danger');
+    // fields.forEach((item, index) => {
+    //   item.innerHTML = `<i class="far fa-clock"></i>`;
+    // });
+  }
+}
+
+// Fetch color info from The Color API
+function fetchColorInfo(hexVal) {
+  // Update info area as fetching..
+  status.innerHTML = `(fetching)`;
+  status.setAttribute('class', 'text-warning');
+  fields.forEach((item, index) => {
+    item.innerHTML = `<i class="far fa-clock"></i>`;
+  });
+
+  // fetch data
+  fetch(`https://www.thecolorapi.com/id?hex=${hexVal}`)
+    .then(res => res.json())
+    .then(data => {
+      updateInfoArea(data);
+    })
+    .catch(err => {
+      console.error(err);
+      updateInfoArea(null);
+    });
+}
+
+// Update hexHistoryQueue
+function updateHistory(hexValue) {
+  if (hexHistoryQueue.length < 11) {
+    hexHistoryQueue.push(hexValue);
+  } else {
+    hexHistoryQueue.shift();
+    hexHistoryQueue.push(hexValue);
+  }
+}
+
+// Update history in localstorage
+function updateStorage() {
+  localStorage.setItem('colors', JSON.stringify(hexHistoryQueue));
+}
+
+// Update the history area
+function updateHistoryArea() {
+  // Remove existing items from UI
+  while (historyContent.firstChild) {
+    historyContent.removeChild(historyContent.firstChild);
+  }
+  // Add contents of hexHistoryQueue to UI
+  hexHistoryQueue.forEach(item => {
+    const historyItem = document.createElement('div');
+    historyItem.setAttribute(
+      'class',
+      'history-item d-flex justify-content-between'
+    );
+    historyItem.innerHTML = `
+      <div class="history-hex p-1">${item}</div>
+      <div
+        class="hex-preview p-1"
+        style="background: ${item};"
+      ></div>
+      <div class="copy px-2 py-1" id="copy">
+        <i class="fa fa-copy"></i> Copy
+      </div>
+    `;
+    historyContent.appendChild(historyItem);
+  });
+}
+
 // Sets a random colour to the background
 function randomColour() {
   let red = randomNumber();
@@ -29,27 +131,51 @@ function randomColour() {
   let blue = randomNumber();
 
   colour.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  hex.value = '#' + rgbToHex(red) + rgbToHex(green) + rgbToHex(blue);
 
-  hex.value = rgbToHex(red) + rgbToHex(green) + rgbToHex(blue);
+  fetchColorInfo(hex.value.substring(1, 7));
+  updateHistory(hex.value);
+  updateStorage();
+  updateHistoryArea();
 }
+
+// Attaching event listeners
 
 // Sets the colour on load
 document.addEventListener('DOMContentLoaded', function() {
+  getHistoryFromStorage();
   randomColour();
 });
 
-//Copy Hex to clipboard
-function copyHex() {
-  hex.focus();
+// Generate new colour
+generate.addEventListener('click', randomColour);
+
+// Primary copy button event listener
+copy.addEventListener('click', () => {
   hex.select();
   document.execCommand('copy');
+  let temp = hex.value;
+  hex.value = 'Copied!';
+  setTimeout(() => {
+    hex.value = temp;
+  }, 1000);
+});
 
-  //Shows message for a brief amount of time
-  message.style.display = 'block';
-  setTimeout(function() {
-    message.style.display = 'none';
-  }, 500);
-}
+// History area copy button even listener
+historyContent.addEventListener('click', e => {
+  if (e.target.id === 'copy') {
+    let hexNode = e.target.parentNode.childNodes[1];
+    let temp = hex.value;
+    hex.value = hexNode.innerHTML;
+    hex.select();
+    document.execCommand('copy');
+    hex.value = temp;
+    e.target.innerHTML = 'Copied!';
+    setTimeout(() => {
+      e.target.innerHTML = '<i class="fa fa-copy"></i> Copy';
+    }, 1000);
+  }
+});
 
 // Register Service Worker
 // Check that service workers are supported
